6e55fb04823901ab96407fd435ce409679721964
JuliaNezhel <yulyazelikova@gmail.com>
2024-07-31 18:41:02 +0300

    feat: county test

--- src/pages/list-countries/list-countries.tsx
+++ src/pages/list-countries/list-countries.tsx
@@ -1,33 +1,35 @@
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-import { Item } from '@/entities/ui/item'
-import { useGetCountriesQuery } from '@/shared/model/api/countries-api'
-import { ResponsiveAppBar } from '@/widgets/header/header'
+import {Item} from '@/entities/ui/item'
+import {useGetCountriesQuery} from '@/shared/model/api/countries-api'
+import {ResponsiveAppBar} from '@/widgets/header/header'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Stack from '@mui/material/Stack'
 
 export const ListCountries = () => {
-  const { data: countries } = useGetCountriesQuery()
+    const {data: countries} = useGetCountriesQuery()
 
-  return (
-    <div>
-      <ResponsiveAppBar />
-      <Container>
-        <Box sx={{ m: 2, width: '100%' }}>
-          <Stack spacing={2}>
-            {countries?.map(country => {
-              return (
-                <Item key={country.name.common}>
-                  <Link style={{ color: 'black' }} to={`${country.name.common}`}>
-                    {country.name.official}
-                  </Link>
-                </Item>
-              )
-            })}
-          </Stack>
-        </Box>
-      </Container>
-    </div>
-  )
+    return (
+        <div>
+            <ResponsiveAppBar/>
+            <Container>
+                <Box sx={{m: 2, width: '100%'}}>
+                    <Stack spacing={2}>
+                        {countries?.map(country => {
+                            return (
+                                <Item key={country.name.common}>
+                                    <Link to={`${country.name.common}`}>
+                                        <div style={{width: '100%'}}>
+                                            {country.name.official}
+                                        </div>
+                                    </Link>
+                                </Item>
+                            )
+                        })}
+                    </Stack>
+                </Box>
+            </Container>
+        </div>
+    )
 }
